dfdb00308f839072bac4c3b7923948a1abc63d94
simo-fol <simofoli@hotmail.it>
2022-06-24 18:07:09 +0200

    indentation fixed

--- app/jobs/import_colorado.rb
+++ app/jobs/import_colorado.rb
@@ -26,73 +26,72 @@ class ImportColorado < ApplicationJob
         csv = "#{dest_path}/#{now}.csv"
         f = File.open(csv, 'wb') { |fp| fp.write(res.body) }
         sleep 5
-          begin
-            if File.exist?(csv)
-              last_printer = customer_machine.printers.order(job_id: :desc).first
-              CSV.foreach(csv, headers: true, col_sep: ";", skip_blanks: true, encoding: 'windows-1252:utf-8', converters: :numeric) do |row|
-                begin
-                  next if row[14] == 'Deleted'
-                  next if last_printer.present? && row[2] <= last_printer.job_id.to_i
-                  file_name = row[4]
-                  if file_name.include?('#LI')
-                    resource_type = "LineItem"
-                    resource_id = file_name.split('#LI').first.to_i
-                    resource = LineItem.find_by(id: resource_id)
-                  elsif file_name.include?('#AJ')
-                    resource_type = "AggregatedJob"
-                    resource_id = file_name.split('#AJ').first.to_i
-                    resource = AggregatedJob.find_by(id: resource_id)
-                  else
-                    resource_type = nil
-                    resource_id = nil
-                    resource = nil
-                  end
-                  headers = row.headers
-                  ink = ""
-                  headers.each do |header|
-                    if header.include?('inkcolor')
-                      ink += "#{header.gsub('inkcolor', '')}: #{row[header].to_f / 1000.0}; "
-                    end
-                  end
-                  start_at = convert_to_time("#{row[6]} #{row[7]}")
-                  print_time = CustomerMachine.hour_to_seconds(row[8]) + CustomerMachine.hour_to_seconds(row[9])
-                  end_at =  start_at + print_time
-                  details = {
-                    resource_id: resource_id,
-                    resource_type: resource_type,
-                    job_id: row[2],
-                    file_name: file_name,
-                    customer_machine_id: customer_machine.id,
-                    start_at: start_at,
-                    print_time: print_time,
-                    end_at: end_at,
-                    copies: row[18],
-                    material: row[20],
-                    ink: ink
-                  }
-                  PRINTER_LOGGER.info "details = #{details}"
-                  printer = Printer.find_by(details)
-                  if printer.nil?
-                    printer = Printer.create!(details)
-                    Log.create!(kind: 'success', action: "Import Colorado", description: "Caricati dati di stampa per #{file_name}")
-                  end
-                rescue Exception => e
-                  PRINTER_LOGGER.info("errore = #{e.message}")
-                  log_details = { kind: 'error', action: "Import Colorado", description: " row == #{row[2]} #{e.message}" }
-                  if Log.where(log_details).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).size == 0
-                    Log.create!(log_details)
+        begin
+          if File.exist?(csv)
+            last_printer = customer_machine.printers.order(job_id: :desc).first
+            CSV.foreach(csv, headers: true, col_sep: ";", skip_blanks: true, encoding: 'windows-1252:utf-8', converters: :numeric) do |row|
+              begin
+                next if row[14] == 'Deleted'
+                next if last_printer.present? && row[2] <= last_printer.job_id.to_i
+                file_name = row[4]
+                if file_name.include?('#LI')
+                  resource_type = "LineItem"
+                  resource_id = file_name.split('#LI').first.to_i
+                  resource = LineItem.find_by(id: resource_id)
+                elsif file_name.include?('#AJ')
+                  resource_type = "AggregatedJob"
+                  resource_id = file_name.split('#AJ').first.to_i
+                  resource = AggregatedJob.find_by(id: resource_id)
+                else
+                  resource_type = nil
+                  resource_id = nil
+                  resource = nil
+                end
+                headers = row.headers
+                ink = ""
+                headers.each do |header|
+                  if header.include?('inkcolor')
+                    ink += "#{header.gsub('inkcolor', '')}: #{row[header].to_f / 1000.0}; "
                   end
                 end
+                start_at = convert_to_time("#{row[6]} #{row[7]}")
+                print_time = CustomerMachine.hour_to_seconds(row[8]) + CustomerMachine.hour_to_seconds(row[9])
+                end_at =  start_at + print_time
+                details = {
+                  resource_id: resource_id,
+                  resource_type: resource_type,
+                  job_id: row[2],
+                  file_name: file_name,
+                  customer_machine_id: customer_machine.id,
+                  start_at: start_at,
+                  print_time: print_time,
+                  end_at: end_at,
+                  copies: row[18],
+                  material: row[20],
+                  ink: ink
+                }
+                PRINTER_LOGGER.info "details = #{details}"
+                printer = Printer.find_by(details)
+                if printer.nil?
+                  printer = Printer.create!(details)
+                  Log.create!(kind: 'success', action: "Import Colorado", description: "Caricati dati di stampa per #{file_name}")
+                end
+              rescue Exception => e
+                PRINTER_LOGGER.info("errore = #{e.message}")
+                log_details = { kind: 'error', action: "Import Colorado", description: " row == #{row[2]} #{e.message}" }
+                if Log.where(log_details).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).size == 0
+                  Log.create!(log_details)
+                end
               end
-            else
-              raise "File CSV non trovato"
             end
-          rescue Exception => e
-            PRINTER_LOGGER.info("errore = #{e.message}")
-            log_details = { kind: 'error', action: "Import Colorado", description: "#{e.message}" }
-            if Log.where(log_details).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).size == 0
-              Log.create!(log_details)
-            end
+          else
+            raise "File CSV non trovato"
+          end
+        rescue Exception => e
+          PRINTER_LOGGER.info("errore = #{e.message}")
+          log_details = { kind: 'error', action: "Import Colorado", description: "#{e.message}" }
+          if Log.where(log_details).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).size == 0
+            Log.create!(log_details)
           end
         end
       end
