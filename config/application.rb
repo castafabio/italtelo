f994759cb795239bf2a8ba11cc5d9b4c31ad6eb8
simo-fol <simofoli@hotmail.it>
2022-06-28 17:02:21 +0200

    tiny_tds imported

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'tiny_tds'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
