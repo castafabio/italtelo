7b1cd4758ea2e6c50fcdb855c4f69602f06acee9
simo-fol <simone.f@soltechservice.it>
2024-01-30 11:08:38 +0100

    clean_up giornaliero

--- app/jobs/clean_up.rb
+++ app/jobs/clean_up.rb
@@ -4,7 +4,11 @@ class CleanUp < ApplicationJob
 
   def perform
     # Cancello tutti i files caricati più vecchi di due mesi
-    ActiveStorage::Attachment.where(record: LineItem.where(status: 'completed')).where("created_at <= ?", Date.today.end_of_day - 2.months).delete_all
-    ActiveStorage::Attachment.where(record: AggregatedJob.completed).where("created_at <= ?", Date.today.end_of_day - 2.months).delete_all
+    ActiveStorage::Attachment.where(record: LineItem.where(status: 'completed')).where("created_at <= ?", Date.today.end_of_day - 2.months).each do |file|
+       FileUtils.rm ActiveStorage::Blob.service.send(:path_for, file.key)
+    end
+    ActiveStorage::Attachment.where(record: AggregatedJob.completed).where("created_at <= ?", Date.today.end_of_day - 2.months).each do |file|
+       FileUtils.rm ActiveStorage::Blob.service.send(:path_for, file.key)
+    end
   end
 end
